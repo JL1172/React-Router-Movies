cbc9c1d24fc924d4407ec05324a17dc06729b71c
Jacob Lang <jacoblang72@comcast.net>
2023-08-01 23:53:22 -0400

    movingontostretch

--- src/Movies/Movie.js
+++ src/Movies/Movie.js
@@ -1,31 +1,33 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { useParams } from 'react-router-dom';
+import { useParams,useNavigate } from 'react-router-dom';
 
 export default function Movie(props) {
   const [movie, setMovie] = useState();
+  const navigate = useNavigate();
   const {id} = useParams(); //!
-  console.log(id)
+  
   // Change ^^^ that line and use a hook to obtain the :id parameter from the URL
 
   useEffect(() => {
-    axios
-      .get(`http://localhost:5001/api/movies/${id}`) // Study this endpoint with Postman
-      .then(response => {
-        console.log(response)
-        debugger
-        // Study this response with a breakpoint or log statements
-        // and set the response data as the 'movie' slice of state
-      })
-      .catch(error => {
-        console.error(error);
-      });
-    // This effect should run every time time
-    // the `id` changes... How could we do this?
-  }, [id]);
+    async function getData() {
+      const res = await axios.get(`http://localhost:5001/api/movies/${id}`) // Study this endpoint with Postman
+      try {
+        setMovie(res.data)// Study this response with a breakpoint or log statements
+                          // and set the response data as the 'movie' slice of state
+      } catch {
+        console.log(new Error)
+      }
+    }
+    getData(); 
+}, [id]);   // This effect should run every time time
+            // the `id` changes... How could we do this?
+      
 
   // Uncomment this only when you have moved on to the stretch goals
-  // const saveMovie = evt => { }
+  const saveMovie = evt => {
+      
+   }
 
   if (!movie) {
     return <div>Loading movie information...</div>;
@@ -51,7 +53,7 @@ export default function Movie(props) {
           </div>
         ))}
       </div>
-      <div className="save-button">Save</div>
+      <div className="save-button" onClick={saveMovie}>Save</div>
     </div>
   );
 }
--- src/Movies/MovieCard.js
+++ src/Movies/MovieCard.js
@@ -1,5 +1,22 @@
-import React from 'react';
+import { useNavigate } from "react-router-dom";
+import React from "react";
 
-export default function MovieCard (props) {
-  return;
-}
+export default function MovieDetails(props) {
+    const navigate = useNavigate(); 
+    const { title, director, metascore, id } = props.movie;
+    const clickMovie = () => {
+      navigate(`/movie/${id}`)
+    }
+    return (
+      <div onClick = {clickMovie} className="movie-card">
+        <h2>{title}</h2>
+        <div className="movie-director">
+          Director: <em>{director}</em>
+        </div>
+        <div className="movie-metascore">
+          Metascore: <strong>{metascore}</strong>
+        </div>
+      </div>
+    );
+  }
+  
